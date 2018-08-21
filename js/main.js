0e8ec8bb7fb4512295bb812d514a1a3774719963
Tictac <paul.giraudet@gmail.com>
2018-08-21 12:07:05 +0200

    adding commentaries

--- js/main.js
+++ js/main.js
@@ -10,26 +10,32 @@ function showSlides() {
 
     id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 
-    if ((i+1) >= [slider.length]) {i = -1}
+    if ((i+1) >= [slider.length]) {i = -1} //from last to first slide
 
     i++;
 
+    //changing background image of unique div
     document.getElementById("slider_container").style.backgroundImage="url('img/"+slider[i]+".jpeg')";
     document.getElementById("numbertext").innerHTML=(i+1)+"/"+slider.length;
 
+    //cleaning all active
     for (j = 0; j < dots.length; j++) {
       dots[j].className = dots[j].className.replace(" active", "");
     }
 
+    //show actual slide
     dots[i].className += " active";
 
-    // return id;
+
 }
 
 // Next/previous controls
 function controls(n) {
 
+  //reseting timer
   clearTimeout(id);
+
+  //changing actual slide indicator
   dots[i].className = dots[i].className.replace(" active", "");
 
   i+=(n);
@@ -41,6 +47,7 @@ function controls(n) {
 
   dots[i].className += " active";
 
+  //restarting timer
   id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
 
 }
