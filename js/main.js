c3261bbc779e5dbd873381d197ccebca4b02f276
Tictac <paul.giraudet@gmail.com>
2018-08-21 11:53:16 +0200

    adding pagination plus timer reset

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 var slider=["img1","img2","img3","img4","img5"];
 
 var i=-1;
+var dots = document.getElementsByClassName("dot");
+var id=0;
 
+
+//automatic slide function
 function showSlides() {
 
-  var id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+    id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+
+    if ((i+1) >= [slider.length]) {i = -1}
 
     i++;
-    // var dots = document.getElementsByClassName("dot");
 
     document.getElementById("slider_container").style.backgroundImage="url('img/"+slider[i]+".jpeg')";
     document.getElementById("numbertext").innerHTML=(i+1)+"/"+slider.length;
 
-    // for (j = 0; j < dots.length; j++) {
-    //   dots[j].className = dots[j].className.replace(" active", "");
-    // }
-    //
-    // dots[i].className += " active";
+    for (j = 0; j < dots.length; j++) {
+      dots[j].className = dots[j].className.replace(" active", "");
+    }
 
-    if ((i+1) >= [slider.length]) {i = -1}
+    dots[i].className += " active";
 
+    // return id;
 }
-
-showSlides();
 
 // Next/previous controls
 function controls(n) {
+
+  clearTimeout(id);
+  dots[i].className = dots[i].className.replace(" active", "");
+
   i+=(n);
   if (i >= [slider.length]) {i = 0}
   if (i<0){i=(slider.length-1);}
@@ -33,14 +39,29 @@ function controls(n) {
   document.getElementById("slider_container").style.backgroundImage="url('img/"+slider[i]+".jpeg')";
   document.getElementById("numbertext").innerHTML=(i+1)+"/"+slider.length;
 
+  dots[i].className += " active";
+
+  id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+
 }
 
 // dots image controls
-// function currentSlide(n) {
-//   i=n;
-//   document.getElementById("slider_container").style.backgroundImage="url('img/"+slider[i]+".jpeg')";
-//   document.getElementById("numbertext").innerHTML=(i+1)+"/"+slider.length;
-//
-//   dots[i].className += " active";
-//
-// }
+function currentSlide(n) {
+
+  clearTimeout(id);
+
+  dots[i].className = dots[i].className.replace(" active", "");
+
+  i=n;
+  document.getElementById("slider_container").style.backgroundImage="url('img/"+slider[i]+".jpeg')";
+  document.getElementById("numbertext").innerHTML=(i+1)+"/"+slider.length;
+
+  dots[i].className += " active";
+
+  id=setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+
+}
+
+
+//starting function
+showSlides();
